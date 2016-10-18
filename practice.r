b4e50a3f71c284b3b1e770d808a725f5281a2131
Rachna Pannu <pannu.rachna@gmail.com>
2016-10-18 13:55:50 -0400

    Added commented header to practice.r

---
+++ practice.r
@@ -0,0 +1,16 @@
+#What: Software Carpentry Workshop
+#When: October 18th, 2016
+#Who: Rachna Pannu
+
+library(RSQLite)
+
+conns <- dbConnect(SQLite(), dbname='C:/Users/rachna/Desktop/survey.sqlite')
+
+tables <- dbListTables(conns)
+
+
+surveys<-read.csv('C:/Users/Rachna/Desktop/ecology.csv 
+
+taxa <- ordered(surveys$taxa, levels = c('Reptile', 'Rabbit', 'Bird','Rodent'))
+
+surveys[surveys$year %in% seq.int(1980,1990) & surveys$taxa =='Rodent',]
